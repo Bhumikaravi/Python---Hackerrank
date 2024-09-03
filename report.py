de098d2cc92e2661cf0c3f697d63992a03985c31
Bhumikaravi <bhumikaravi2006@gmail.com>
2024-09-03 19:03:57 +0530

    Add files via upload

---
+++ report.py
@@ -0,0 +1,9 @@
+Dead=int(input())
+injured=int(input())
+safe=int(input())
+print('Dead:',Dead)
+print('injured:',injured)
+print('safe:',safe)
+print("please help the people who are suffering")
+      
+
